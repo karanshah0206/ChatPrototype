ed5774e43f1c0a84a8e32988d63ee87ec1f74dd4
Karan Shah <karanshah0206@outlook.com>
2024-06-16 21:16:34 +1000

    Custom Subviews

--- ChatPrototype/ContentView.swift
+++ ChatPrototype/ContentView.swift
@@ -10,17 +10,61 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Knock, knock!")
-                .padding()
-                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
-            Text("Who's there?")
-                .padding()
-                .background(Color.cyan, in: RoundedRectangle(cornerRadius: 8))
+            VStack {
+                Text("Knock, knock!")
+                    .padding()
+                    .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
+                Text("Who's there?")
+                    .padding()
+                    .background(Color.cyan, in: RoundedRectangle(cornerRadius: 8))
+            }
+            .padding()
+
+            HStack {
+                DayForecast(day: "Mon", high: 14, low: 8)
+                DayForecast(day: "Tue", high: 12, low: 7, isRainy: true)
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct DayForecast: View {
+    let day: String
+    let high: Int
+    let low: Int
+    let isRainy: Bool
+
+    var iconName: String {
+        isRainy ? "cloud.rain.fill" : "sun.max.fill"
+    }
+
+    var iconColor: Color {
+        isRainy ? .blue : .yellow
+    }
+    
+    init (day: String, high: Int, low: Int, isRainy: Bool = false) {
+        self.day = day
+        self.high = high
+        self.low = low
+        self.isRainy = isRainy
+    }
+
+    var body: some View {
+        VStack {
+            Text(day)
+                .font(.headline)
+            Image(systemName: iconName)
+                .foregroundColor(iconColor)
+                .font(.largeTitle)
+                .padding(5)
+            Text("High: \(high)")
+            Text("Low: \(low)")
+                .foregroundStyle(Color.secondary)
+        }
+        .padding()
+    }
 }
